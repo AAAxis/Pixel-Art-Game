feee40f015862f4e16045bbbe00ed1b3949934d6
AAAxis <polskoydm@gmail.com>
2022-02-23 17:46:32 +0200

    Pixel Crew 1.3
    Added Scripts
    - Hero Input Reader
    - Hero

---
+++ Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeroInputReader : MonoBehaviour
+    {
+        [SerializeField] private Hero _hero;
+
+        public void OnMovement(InputAction.CallbackContext context)
+        {
+            var direction = context.ReadValue<Vector2>();
+            _hero.SetDirection(direction);
+        }
+
+        public void OnSaySomething(InputAction.CallbackContext context)
+        {
+            if (context.phase == InputActionPhase.Canceled)
+            {
+                _hero.SaySomething();
+            }
+        }
+    }   
