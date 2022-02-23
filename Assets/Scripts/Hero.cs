5d25f8dc58ffc1bebf4b69f333840c59e76108fa
AAAxis <polskoydm@gmail.com>
2022-02-23 17:58:51 +0200

    Pixel Crew 1.6
    Added Animation
    - Sprite Animation Script
    - Jump, Run, Hit

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -5,22 +5,20 @@ public class Hero : MonoBehaviour
 {
     [SerializeField] private float _speed = 1f;
     [SerializeField] private float _jumpPower = 1f;
+    [SerializeField] private LayerCheck _groundCheck;
    
-    
-   
-
+    private Animator _animator;
+    private SpriteRenderer _sprite;
     private Rigidbody2D _rigidbody;
     private Vector2 _direction;
-
-    [SerializeField] private LayerCheck _groundCheck;
+    
     public static int totalScore;
- 
-   
-
 
     private void Awake()
     {
+        _animator = GetComponent<Animator>();
         _rigidbody = GetComponent<Rigidbody2D>();
+        _sprite = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
@@ -28,9 +26,11 @@ public class Hero : MonoBehaviour
         _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
 
         var isJumping = _direction.y > 0;
+        var isGrounded = IsGrounded();
+
         if (isJumping)
         {
-            if (IsGrounded())
+            if (isGrounded && _rigidbody.velocity.y <= 0.1f)
             {
                 _rigidbody.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
             }
@@ -40,12 +40,26 @@ public class Hero : MonoBehaviour
         {
             _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
         }
+
+            _animator.SetBool("is -running", _direction.x != 0);
+            _animator.SetBool("is -ground", isGrounded);
+            _animator.SetFloat("vertical -velocity", _rigidbody.velocity.y);
+
+            UpdateSpriteDirection();
+
     }
-  
-    
-    
-   
-    
+
+    private void UpdateSpriteDirection()
+        {
+            if (_direction.x > 0)
+            {
+                _sprite.flipX = false;
+            }
+            else if (_direction.x < 0)
+            {
+                _sprite.flipX = true;
+            }
+        }
 
     private bool IsGrounded()
     {
